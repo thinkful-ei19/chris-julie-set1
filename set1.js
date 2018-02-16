5925133c68211525eca5fbb687ec7c426142bc3c
cmilkaitis <chris.milkaitis@gmail.com>
2018-02-16 17:46:33 -0500

    working set1

--- set1.js
+++ set1.js
@@ -1,14 +1,29 @@
 'use strict';
 
 const yearOfBirth = function(age) {
-  return 2018 - age;
+  if(typeof age === 'number') {
+    if(age < 0) {
+      throw new Error('Age can not be negative');
+    } else {
+      return 2018 - age;
+    }
+  } else {
+    throw new Error('Age is not a number');
+  }
 };
 
 const whoAmI = function(name, age) {
   let yob = yearOfBirth(age);
-  console.log(`Hi, my name is ${name} and I\'m ${age} years old`);
+  if (name === undefined || age === undefined)
+    throw new Error('Arguments not defined');
+  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
   console.log(`I was born in ${yob}`);
 };
 
 
-whoAmI("Chris", 40);
+try { 
+  whoAmI('chris', 40);
+}
+catch(error) {
+  console.log(error.message);
+}
