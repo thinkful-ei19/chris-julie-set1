c45f45f1122f53050e1418d4ed611ee19c2c92d9
Julie K <julieskim160@gmail.com>
2018-02-19 11:44:41 -0800

    yearOfBirth

--- set1.js
+++ set1.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const whoAmI = function() {
+const whoAmI = function(name, age) {
+  let yearOfBirth = 2018 - age;
   console.log('Hi, my name is Julie and I\'m 29 years old');
+  console.log('I was born in ' + yearOfBirth);
 };
 
 whoAmI();
